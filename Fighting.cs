e6f1368d956a91c371c06ad909c19301324421e8
IGOR3356X <igor3356c@gmail.com>
2024-02-15 12:50:56 +0300

    RPG Update

---
+++ Fighting.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik
+{
+    internal class Fighting
+    {
+        
+    }
+    class Basestats
+    {
+        protected int HP;
+        protected int Armor;
+        protected int Damage;
+
+        public Basestats(int hp, int armor, int damage)
+        {
+            HP = hp;
+            Armor = armor;
+            Damage = damage;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            int Zahita = Armor - damage;
+            if (Zahita < 0)
+            {
+                HP += Zahita;
+                Armor = 0;
+                Zahita -= Zahita * 2;
+                Console.WriteLine($"Броня сломана, получено урона {Zahita}");
+            }
+            else
+            {
+                Console.WriteLine("Урон не прошёл");
+            }
+        }
+    }
+    class Knight : Basestats
+    {
+        public Knight() : base(100, 50, 15) { }
+        public void AvalonHeal() 
+        {
+            HP += 20;
+            Console.WriteLine($"Рыцарь использовал авалон его текущее ХП = {HP}");
+        }
+        public void ShowInfo()
+        {
+            if (HP < 0)
+                Console.WriteLine("Рыцарь умер");
+            else
+                Console.WriteLine($"Текущее зоровье рыцаря: {HP}");
+        }
+    }
+    class Caster : Basestats
+    {
+        public Caster(): base(70, 25, 30) { }
+
+        public void DamageBaff() 
+        {
+            Damage += 10;
+            Console.WriteLine($"Кастер использовал усиление заклинаний его урон = {Damage}");
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Текущее зоровье кастера {HP}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,11 +7,29 @@ namespace Stepik
     {
         static void Main()
         {
-            PlayerInfo GoodGame = new PlayerInfo();
-            PlayerInfo[] BayBay = { new PlayerInfo("Тайлер Дырден", 100, 50, 5), new PlayerInfo("Тайлер Дырден2", 50, 20, 2) };
-            for (int i = 0; i < BayBay.Length; i++)
+            //PlayerInfo GoodGame = new PlayerInfo();
+            //PlayerInfo[] BayBay = { new PlayerInfo("Тайлер Дырден", 100, 50, 5), new PlayerInfo("Тайлер Дырден2", 50, 20, 2) };
+            //for (int i = 0; i < BayBay.Length; i++)
+            //{
+            //    BayBay[i].ConsoleBitch();
+            //}
+            while (true)
             {
-                BayBay[i].ConsoleBitch();
+                Knight warrior1 = new Knight();
+                Caster warrior2 = new Caster();
+
+                warrior1.TakeDamage(50);
+                warrior2.TakeDamage(50);
+                warrior1.ShowInfo();
+                warrior2.ShowInfo();
+
+                ConsoleKeyInfo AbylityK = Console.ReadKey();
+                ConsoleKeyInfo AbylityC = Console.ReadKey();
+
+                if (AbylityK.Key == ConsoleKey.A)
+                    warrior1.AvalonHeal();
+                else if (AbylityC.Key == ConsoleKey.D)
+                    warrior2.DamageBaff();
             }
         }
     }
